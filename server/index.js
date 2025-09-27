dcf63d9c6f7207fb06a395e55ba4618b65638c28
zarwaan <zarwaanshroff@gmail.com>
2025-09-27 20:38:29 +0530

    minor change

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const { Server } = require("socket.io");
 const cors = require("cors");
 
 const app = express();
-app.use(cors());
+app.use(cors({origin: "*" }));
 
 const server = http.createServer(app);
 
