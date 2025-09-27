e1835e14f0347bcbcfb65e774a63e5bdacd4c48c
zarwaan <zarwaanshroff@gmail.com>
2025-09-27 20:49:09 +0530

    updated socketprovider with deployed backend link

--- client/src/context/SocketProvider.jsx
+++ client/src/context/SocketProvider.jsx
@@ -9,7 +9,7 @@ export const useSocket = () => {
 };
 
 export const SocketProvider = (props) => {
-  const socket = useMemo(() => io("localhost:8000"), []);
+  const socket = useMemo(() => io("https://ia1-submission-team-titans-2d6s.onrender.com/"), []);
 
   return (
     <SocketContext.Provider value={socket}>
